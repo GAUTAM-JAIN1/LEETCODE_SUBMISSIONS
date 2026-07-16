6632c9036db8dcf652893bdbf52cbad2d6f97313
Gautam Jain <gautamjainjind@gmail.com>
2026-07-16 10:55:02 +0530

    Time: 0 ms (100%), Space: 7.8 MB (84.75%) - LeetHub

--- 0788-rotated-digits/0788-rotated-digits.cpp
+++ 0788-rotated-digits/0788-rotated-digits.cpp
@@ -1,32 +1,55 @@
 class Solution {
 public:
-
-    bool helper(int a){
-            int m = a;
-            vector<int>arr;
-            while(m>0){
-                int b = m%10;
-                arr.push_back(b);
-                m/=10;
-            }
-            vector<int>ans;
-            for(int i=0;i<arr.size();i++){
-                if(arr[i]==3 || arr[i]==4 || arr[i]==7)return false;
-                else if(arr[i]==0 || arr[i]==1 || arr[i]==8)ans.push_back(arr[i]);
-                else if(arr[i]==2)ans.push_back(5);
-                else if(arr[i]==5)ans.push_back(2);
-                else if(arr[i]==6)ans.push_back(9);
-                else if(arr[i]==9)ans.push_back(6);
-            }
-            if(arr == ans)return false;
-            return true;
-    }
-
     int rotatedDigits(int n) {
-    int count=0;
-            for(int i=1;i<=n;i++){
-               if(helper(i))count++; 
+        int ans = 0;
+        for (int i = 1; i <= n; i++) {
+            int x = i;
+            bool valid = true;
+            bool changed = false;
+            while (x > 0) {
+                int d = x % 10;
+                if (d == 3 || d == 4 || d == 7) {
+                    valid = false;
+                    break;
+                }
+                if (d == 2 || d == 5 || d == 6 || d == 9) {
+                    changed = true;
+                }
+                x /= 10;
             }
-            return count;
+
+            if (valid && changed)
+                ans++;
+        }
+
+        return ans;
     }
 };
+
+/* bool helper(int a){
+         int m = a;
+         vector<int>arr;
+         while(m>0){
+             int b = m%10;
+             arr.push_back(b);
+             m/=10;
+         }
+         vector<int>ans;
+         for(int i=0;i<arr.size();i++){
+             if(arr[i]==3 || arr[i]==4 || arr[i]==7)return false;
+             else if(arr[i]==0 || arr[i]==1 || arr[i]==8)ans.push_back(arr[i]);
+             else if(arr[i]==2)ans.push_back(5);
+             else if(arr[i]==5)ans.push_back(2);
+             else if(arr[i]==6)ans.push_back(9);
+             else if(arr[i]==9)ans.push_back(6);
+         }
+         if(arr == ans)return false;
+         return true;
+ }
+
+ int rotatedDigits(int n) {
+ int count=0;
+         for(int i=1;i<=n;i++){
+            if(helper(i))count++;
+         }
+         return count;*/
